6d1a6e8c72c40989fce6a3e5884183a5c8a4e659
PeninahMuyela <muyelap@gmail.com>
2023-03-19 14:02:56 -0700

    Hello World task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - This is a C program that prints using the puts function
+ *Return: 0 (success)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
