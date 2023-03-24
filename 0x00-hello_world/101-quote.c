e79a3f62bffa929b390fa3deb6aefeb4fe7e3372
PeninahMuyela <muyelap@gmail.com>
2023-03-24 08:17:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a C program that prints a line with the standard error
- *
- * return: 1 (success)
+ * main - entry point
+ * description:  and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: Always (1) Success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
